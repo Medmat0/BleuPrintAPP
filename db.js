7407a75722efcef2e5ff7eb6ad422a2e3ba442ac
Medmat0 <yassineelmatrour@gmail.com>
2023-11-22 13:01:25 +0100

    Yaml version final

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database url
+  process.env.DBfirstBlue_URL, // TODO: database url
   {
     dialect: 'postgres',
     dialectOptions: {
